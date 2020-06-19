f8dcb60d8cefe7cc31be3e7c0927e32bf0284e19
dsplvd <d@splvd.com>
2020-06-18 20:18:20 -0400

    first blood

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export DISPLAY=:0.0
+sleep 45 && ~/mesamoller-launcher/node_modules/electron/dist/electron ~/mesamoller-launcher/.
---
+++ screen.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export DISPLAY=:0.0
+xinput set-prop  'Weida Hi-Tech                CoolTouchR System           ' 'Coordinate Transformation Matrix' 0 -1 1 1 0 0 0 0 1
+xset s off
+xset -dpms
